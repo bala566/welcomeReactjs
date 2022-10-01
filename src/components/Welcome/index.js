134f52daa9045d435fbb9750c7304b7e15c04540
bala566 <baladurga566@gmail.com>
2022-10-01 15:49:33 +0000

    welcome app usign reactjs

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,32 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class Welcome extends Component {
+  state = {isSubscribed: false}
+
+  onSubscribe = () => {
+    this.setState(prevState => ({isSubscribed: !prevState.isSubscribed}))
+  }
+
+  getButtonText = () => {
+    const {isSubscribed} = this.state
+
+    return isSubscribed ? 'Subscribed' : 'Subscribe'
+  }
+
+  render() {
+    const buttonText = this.getButtonText()
+    return (
+      <div className="container">
+        <h1 className="heading">WELCOME</h1>
+        <p className="paragraph">Thank you! Happy Learning</p>
+        <button type="button" className="button" onClick={this.onSubscribe}>
+          {buttonText}
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Welcome
